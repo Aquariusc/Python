83204060f24ceb911fa1917ad992def08c6daa46
Aquariusc <449204451@qq.com>
2018-01-15 12:35:53 +0800

    18.1.15
    A-Byte-of-Python , examples of chapter 11

---
+++ Method.py
@@ -0,0 +1,11 @@
+#!/E/Lucifer/Python/Code
+#!Filename:Method.py
+
+class Person:
+    def sayHi(self):
+        print('Hello,how are you?')
+
+p=Person()
+p.sayHi()
+
+#This short example can also be written as Person().sayHi()
---
+++ Simplestclass.py
@@ -0,0 +1,8 @@
+#!/E/Lucifer/Python/Code
+#!Filename:Simplestclass.py
+
+class Person:
+    pass #An empty block
+
+p=Person()
+print(p)
